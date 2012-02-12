6ba5a0e572c9b45cd38fb45ce591b9f9cdd34b28
Fabien Sanglard <sanglardf@yahoo.fr>
2012-02-12 14:59:21 -0500

    Start Android music playback wiht OpenES.

--- engine/android/jni/android/android_native.c
+++ engine/android/jni/android/android_native.c
@@ -1,12 +1,13 @@
 
 
 // music.h placeholders
+/*
 void SND_InitSoundTrack(char* filename,unsigned int startAt){}
 void SND_StartSoundTrack(void){}
 void SND_StopSoundTrack(void){}
 void SND_PauseSoundTrack(void){}
 void SND_ResumeSoundTrack(void){}
-
+*/
 
 // native_service.h
 int  Native_RetrieveListOf(char replayList[10][256]){ return 0;}
